05034c8da00aaafb2f91183bca5875e953fc2aad
sleeve <1421147715@qq.com>
2019-09-19 21:37:43 +0800

    yield和join的区别

--- java_project/src/Thread2/BlockJoin01.java
+++ java_project/src/Thread2/BlockJoin01.java
@@ -1,7 +1,9 @@
 package Thread2;
 /**
  * jion()方法：线程实例的join()方法可以使得一个线程在另一个线程结束后再执行，即也就是说使得当前线程可以阻塞其他线程执行
- * 当前线程阻塞，其他执行完之后，再执行。
+ * thread.Join把指定的线程加入到当前线程，可以将两个交替执行的线程合并为顺序执行的线程。
+ *
+ * 比如在线程B中调用了线程A的Join()方法，直到线程A执行完毕后，才会继续执行线程B。
  */
 public class BlockJoin01 {
     public static void main(String[] args) throws InterruptedException {
