71c4ccffd291c614cfd4ca52c1c3650f6690b864
sleeve <1421147715@qq.com>
2019-09-22 22:28:12 +0800

    同步方法

---
+++ java_project/src/Thread_study03/Account.java
@@ -0,0 +1,11 @@
+package Thread_study03;
+
+public class Account {
+    int money;
+    String name;
+
+    Account(int money, String name) {
+        this.money = money;
+        this.name = name;
+    }
+}
--- java_project/src/Thread_study03/SynTset02.java
+++ java_project/src/Thread_study03/SynTset02.java
@@ -1,37 +1,45 @@
 package Thread_study03;
 
 /**
- * 取钱，线程不安全
+ * 取钱
+ * 线程安全:在并发时，保证数据正确性，尽可能效率高
+ * synchronized
+ * 1.同步方法
+ * 2.同步块
  */
-public class UnsafeTset02 {
+public class SynTset02 {
     public static void main(String[] args) {
         Account account = new Account(100,"礼金");
-        Drawing you = new Drawing(account,80,"可悲的你");
-        Drawing wifi = new Drawing(account,90,"happy的她");
+        SafeDrawing you = new SafeDrawing(account,80,"可悲的你");
+        SafeDrawing wifi = new SafeDrawing(account,90,"happy的她");
         you.start();
         wifi.start();
 
     }
 }
 
-
 //模拟取款机
-class Drawing extends Thread{
+class SafeDrawing extends Thread{
     private Account account;//取钱账户
     private int drawMoney;//取得钱
     private int packetToal;//口袋的钱
 
-   public Drawing(Account account, int drawMoney, String name) {
+   public SafeDrawing(Account account, int drawMoney, String name) {
         super(name);
         this.account = account;
         this.drawMoney = drawMoney;
     }
 
     @Override
-    public void run() {
-       if (account.money-drawMoney<0){
-           return;
-       }
+    public  void run() {
+       test();
+    }
+
+    //目标不对，锁定失败,这里不是锁this，是锁account
+    private synchronized void test(){
+        if (account.money-drawMoney<0){
+            return;
+        }
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
