68989f65b47625d8576cec68485f3f51e9e9b7fc
sleeve <1421147715@qq.com>
2019-11-06 14:51:32 +0800

    SORM数据库类型转java类型

--- JDBC/src/Big_SORM/JavaFileUtils.java
+++ JDBC/src/Big_SORM/JavaFileUtils.java
@@ -4,4 +4,38 @@ package Big_SORM;
  * 封装了生成java源文件操作
  */
 public class JavaFileUtils {
+    /**
+     * 根据字段信息生成java属性信息。如 bar username->private String username
+     * @param column 字段信息
+     * @param convertor 类型转化其
+     * @return 返回java属性和set/get方法源码
+     */
+    public static JavaFieldGetSet createFieldGetSet(ColumnInfo column, TypeConverrtor convertor){
+        JavaFieldGetSet jfgs = new JavaFieldGetSet();
+        String javaFieldType = convertor.databaseType2JavaType(column.getDataType());
+        jfgs.setFieldInfo("\tprivate "+javaFieldType+" "+column.getName()+";\n");
+        //public String getUsername(){return username};
+        //生成get源码
+        StringBuilder getSrc = new StringBuilder();
+        getSrc.append("\tpublic "+ javaFieldType+" get"+
+                StringUtils.firstChar2UpperCase(column.getName())+"()\n");
+        getSrc.append("\treturn "+column.getName()+";\n");
+        getSrc.append("\t}\n");
+        jfgs.setGetdInfo(getSrc.toString());
+        //生成set源码
+        //public void setUsername(String username){this.username=username}
+        StringBuilder setSrc = new StringBuilder();
+        setSrc.append("\tpublic void set"+ StringUtils.firstChar2UpperCase(column.getName())+"(");
+        setSrc.append(javaFieldType+" "+column.getName()+"){\n");
+        setSrc.append("\tthis."+column.getName()+"="+column.getName()+";\n");
+        setSrc.append("\t}\n");
+        jfgs.setSetInfo(setSrc.toString());
+        return jfgs;
+    }
+
+    public static void main(String[] args) {
+        ColumnInfo ci = new ColumnInfo("username","varchar",0);
+        JavaFieldGetSet f = createFieldGetSet(ci, new MySQLTypeConbertor());
+        System.out.println(f);
+    }
 }
--- JDBC/src/Big_SORM/StringUtils.java
+++ JDBC/src/Big_SORM/StringUtils.java
@@ -4,4 +4,14 @@ package Big_SORM;
  * 封装字符串常用操作
  */
 public class StringUtils {
+    /**
+     * 首字母转大写
+     * @param str
+     * @return
+     */
+    public static String firstChar2UpperCase(String str){
+        return str.substring(0,1).toUpperCase()+str.substring(1);
+
+
+    }
 }
