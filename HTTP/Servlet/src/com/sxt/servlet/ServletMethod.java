71849aaafde411ef8c33ef4810f0fcdbb65e9d5e
sleeve <1421147715@qq.com>
2019-11-13 19:38:51 +0800

    Update ServletMethod.java

--- HTTP/Servlet/src/com/sxt/servlet/ServletMethod.java
+++ HTTP/Servlet/src/com/sxt/servlet/ServletMethod.java
@@ -5,8 +5,17 @@ package com.sxt.servlet;
  * doGet：只可以接受get的请求
  * doPost：值可以接受post的请求
  * 注意：如果在重写的service方法中调用了父类的service方法（spuer.service（xxx,xxx））
- * 则service方法处理完后，会再次根据请求方式响应doget或dopost。
- * 所以一般情况下，不在重写的service中保留super。
+ *       则service方法处理完后，会再次根据请求方式响应doget或dopost。
+ *      所以一般情况下，不在重写的service中保留super。
+ * Servlet常见错误：
+ * 404错误：资源未找到
+ * 原因一：在请求地址中servlet的别名错误
+ * 原因二：虚拟项目拼写错误
+ * 500错误：内部服务器错误
+ *      1.在web.xml中校验sevlet类的全限定路劲拼写错误
+ *      2.service方法体执行代码逻辑存在逻辑错误
+ * 405错误：请求方式不支持
+ *      请求方式和servlet中的方法不匹配
  */
 
 import javax.servlet.ServletException;
