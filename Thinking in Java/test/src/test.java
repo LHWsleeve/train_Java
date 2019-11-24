6c59de7c63c12f10a07625ce066596f74a7789ec
sleeve <1421147715@qq.com>
2019-11-24 23:34:05 +0800

    test

---
+++ Thinking in Java/test/src/test.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class test {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for(int i=10; i<100; i++){
+            for (int j=i+1;j<100;j++) {
+               int c =i*j;
+                if (c < 1000 || c > 9999) {
+                    continue;
+                }
+                list.add(i % 10);//各位
+                list.add(i / 10);//十位
+                list.add(j % 10);//各位
+                list.add(j / 10);//十位
+                list2.add(c % 10);//
+                list2.add(c / 10 % 10);//
+                list2.add(c / 100 % 10);//
+                list2.add(c / 1000 % 10);//
+                Collections.sort(list);//排序
+                Collections.sort(list2);
+                if (list.equals(list2)){
+                System.out.println("i:"+i+" j:"+j+" c:"+c);
+                }
+                list.clear();
+                list2.clear();
+            }
+        }
+    }
+}
