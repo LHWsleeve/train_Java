128cb4eb7159a5b2656ee3c9f65da043c1f923ab
sleeve <1421147715@qq.com>
2019-12-13 14:56:58 +0800

    JDBC

--- SSM/S/JDBC/src/test/java/com/guigu/JDBCTemplate/AppTest.java
+++ SSM/S/JDBC/src/test/java/com/guigu/JDBCTemplate/AppTest.java
@@ -3,18 +3,34 @@ package com.guigu.JDBCTemplate;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void JDBCTest() throws SQLException {
+        DataSource bean = ioc.getBean(DataSource.class);
+        Connection connection = bean.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+    @Test
+    public void JDBCTest02(){
+        String sql = "update user set salary=? where id=?";
+        JdbcTemplate bean = (JdbcTemplate) ioc.getBean("jdbcTemplate");
+        int update = bean.update(sql, 1300, 5);
+        System.out.println(update);
     }
 }
