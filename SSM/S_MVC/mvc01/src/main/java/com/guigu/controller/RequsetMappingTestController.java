720f86d522ad22e1a7077ad7eb872280a693f0e7
sleeve <1421147715@qq.com>
2019-12-17 20:02:53 +0800

    ResquestMapping-规定请求方式01（Method）

--- SSM/S_MVC/mvc01/src/main/java/com/guigu/controller/RequsetMappingTestController.java
+++ SSM/S_MVC/mvc01/src/main/java/com/guigu/controller/RequsetMappingTestController.java
@@ -3,6 +3,7 @@ package com.guigu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * RequestMapping加载类之前，为当前类的所有方法指定一个基准方法
@@ -13,6 +14,22 @@ public class RequsetMappingTestController {
     @RequestMapping("/handle01")
     public String handle01(){
         System.out.println("RequsetMappingTestController----handlw01--被调用了");
+        return "success";
+    }
+
+    /**
+     * ResquestMapping的其他属性
+     * method:限定请求方式
+     *          GET,POST....只接收这种类型的请求，默认全接受
+     *          不是规定的方式就会报错：HTTP Status 405 – Method Not Allowed
+     * params:
+     * headers
+     * consumes:
+     * prodouces:
+     */
+    @RequestMapping(value = "/handle02",method = RequestMethod.POST)
+    public String handle02(){
+        System.out.println("02成功");
         return "success";
     }
 
--- SSM/S_MVC/mvc01/target/mvc01/index.jsp
+++ SSM/S_MVC/mvc01/target/mvc01/index.jsp
@@ -7,6 +7,11 @@
 <a href="/handle01">test01-写在方法上的ResquestMapping测试</a>
 <br/>
 <a href="haha/handle01">test01-写在l类上的ResquestMapping测试</a>
-
+<br/>
+<h1>测试ResquestMapping的属性</h1>
+<a href="/haha/handle02">handle02</a><br/>
+<form action="/haha/handle02" method="post">
+    <input type="submit">
+</form>
 </body>
 </html>
