c63846bc658d2d064d26c1235e051674cbba934f
sleeve <1421147715@qq.com>
2020-01-04 22:02:22 +0800

    前缀转发和重定向

--- SSM/S_MVC/mvc03/src/main/java/com/guigu/Controller/HelloController.java
+++ SSM/S_MVC/mvc03/src/main/java/com/guigu/Controller/HelloController.java
@@ -2,12 +2,55 @@ package com.guigu.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+//     redirect，foward:有前缀的转发，不会有我们配置的视图解析器拼串
 
 @Controller
 public class HelloController {
 
+    //执行hello方法后，return到hello页面。直接return，视图解析器会自动拼串导致回到/WEB-INF/page/xx,但是hello不在该文件夹下
     @RequestMapping("/hello")
     public String hello(){
-        return "success";
+        //1.使用相对路径
+        return "../../hello";
+    }
+
+    /**
+     * foward:转发一个页面
+     * foward:前缀的转发，不会有我们配置的视图解析器拼串
+     * 一定加上/，如果没有/，就是相对路径容易错误
+     * /hello.jsp：转发当前项目下的hello.jsp，当前webapp下
+     * @return
+     */
+    @RequestMapping("/handle01")
+    public String handle01(){
+        System.out.println("handle01");
+        //2.使用forward
+        return "forward:/hello.jsp";
+    }
+    @RequestMapping("/handle02")
+    public String handle02(){
+        System.out.println("handle02");
+        //2.使用forward,多次派发。handle02-->handle01-->hello.jsp
+        return "forward:/handle01";
+    }
+
+    /**
+     * 重定向到he11o.jsp页面
+     * 转发 forward：转发的路径
+     * 重定向 redirect：重定向的路径。SpringMVC会为路径拼接上项目名可以直接写/hello.jsp
+     * 原生Servlet重定向需要加上项目名才能重定向，下面这种方式不行
+     *      response.sendRedirect("/hello.jsp")
+     * @return
+     */
+    @RequestMapping("/handle03")
+    public String handle03() {
+        System.out.println("handle03");
+        return "redirect:/hello.jsp";
+    }
+
+    @RequestMapping("/handle04")
+    public String handle04() {
+        System.out.println("handle04");
+        return "redirect:/handle03";
     }
 }
--- SSM/S_MVC/mvc03/src/main/webapp/index.jsp
+++ SSM/S_MVC/mvc03/src/main/webapp/index.jsp
@@ -1,6 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"
+         isErrorPage="true" isELIgnored="false" %>
 <html>
 <body>
 <h2>Hello World!</h2>
 </body>
-<a href="hello">Hello</a>
+<a href="hello">Hello</a></br>
+<a href="/handle01">handle01去hello页面</a></br>
+<a href="/handle02">forward多派发测试</a></br>
+<a href="/handle03">redirect重定向测试</a></br>
+<a href="/handle04">redirect多次重定向测试</a></br>
+
 </html>
