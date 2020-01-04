aa125945b410835fd12bb22e3857ba85d3438ccd
sleeve <1421147715@qq.com>
2020-01-04 21:13:42 +0800

    视图解析器-01Helloworld

---
+++ SSM/S_MVC/mvc03/src/main/java/com/guigu/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.guigu.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "success";
+    }
+}
