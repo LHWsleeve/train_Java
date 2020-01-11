7c5255756d73cca2ab3930abbeef54e1df6a769d
sleeve <1421147715@qq.com>
2020-01-11 15:39:04 +0800

    返回自定义响应

--- SSM/S_MVC/Springmvc_curd/src/main/java/com/guigu/Controller/AjaxController.java
+++ SSM/S_MVC/Springmvc_curd/src/main/java/com/guigu/Controller/AjaxController.java
@@ -4,12 +4,17 @@ import com.guigu.dao.EmployeeDao;
 import com.guigu.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.print.DocFlavor;
+import java.security.PublicKey;
 import java.util.Collection;
 
 @Controller
@@ -61,5 +66,23 @@ public class AjaxController {
     public String testRequestBody2(@RequestBody Employee employee){
         System.out.format("请求体"+employee);
         return "success";
+    }
+
+    /**
+     *     @ResponseBody:本质的含义是将返回的数据放在响应体中。
+     *     在本例中，加了该注解后，直接访问testResponseBody，把本来应该返回success页面，
+     *     但是实际上是吧“success”字符串打印在页面中。
+     *     不再参与视图解析器
+     * @return
+     */
+    @RequestMapping("/testResponseBody")
+//    @ResponseBody
+    public ResponseEntity<String> testResponseBody(){
+        System.out.format("haha");
+        HttpStatus statusCode;
+        String body="<h1>success<h1>";
+        MultiValueMap<String, String> header = new HttpHeaders();
+        header.add("Set-Cookie","username=hahahah");
+        return new ResponseEntity<String>(body, header, HttpStatus.OK);
     }
 }
